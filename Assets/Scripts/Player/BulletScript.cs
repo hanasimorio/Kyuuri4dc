1e7deb4a487bb17917d1a26e6628fc7fef5b3755
nerumoa019 <50538677+nerumoa019@users.noreply.github.com>
2022-02-10 19:07:06 +0900

    プレイヤーのジャンプ追加など

--- Assets/Scripts/Player/BulletScript.cs
+++ Assets/Scripts/Player/BulletScript.cs
@@ -4,12 +4,42 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    [SerializeField] private float speed = default;
+    private Vector3 velo;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void FixedUpdate()
     {
-        transform.Translate(speed * Time.deltaTime, 0f, 0f);
+        //rb.velocity = GetVelocity(angle) * speed;
+        //transform.position += velo * Time.deltaTime;
+        //transform.Translate(speed * Time.deltaTime, 0f, 0f);
     }
+
+    /*
+    public void Init(float angle, float speed)
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = GetVelocity(angle);
+        //velo = transform.TransformDirection(new Vector3(speed, speed, 0f).normalized);
+        //Debug.Log(transform.rotation);
+        //Debug.Log(velo);
+    }
+
+    private static Vector3 GetVelocity(float angle)
+    {
+        return new Vector3
+        (
+            Mathf.Cos(angle * Mathf.Deg2Rad),
+            Mathf.Sin(angle * Mathf.Deg2Rad),
+            0
+        );
+    }
+    */
+
 
     private void OnBecameInvisible()
     {
--- Assets/Scripts/Player/PlayerMoveScript.cs
+++ Assets/Scripts/Player/PlayerMoveScript.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class PlayerMoveScript : MonoBehaviour
 {
-    [SerializeField] private GameObject bullet = default;
-    [SerializeField] private float speed = default;
+    enum Status
+    {
+        GROUND,
+        RISE,
+        FALL
+    }
 
-    private float xSpeed, ySpeed;
-    private bool isShot;
-    private Rigidbody2D rb;
-    private Vector2 dir;
+    [SerializeField] LayerMask groundLayer = default;
+    [SerializeField] BulletScript bullet = default;
+
+    float xRate;
+    float speed = 5f;
+    bool isMove = false;
+
+    float jumpTimer = 0f;
+    const float jumpPower = 12f;
+    const float gravity = 100f;
+    bool isJump = false;
+    bool jumpKey = false;
+    bool jumpKeyLock = false;
+
+    bool isShot = false;
+
+    Rigidbody2D rb;
+    Vector2 vect;
+    Status status = Status.GROUND;
 
     private void Start()
     {
@@ -19,17 +38,96 @@ public class PlayerMoveScript : MonoBehaviour
 
     private void Update()
     {
-        xSpeed = Input.GetAxisRaw("Horizontal");
-        ySpeed = Input.GetAxisRaw("Vertical");
-        isShot = Input.GetKey("space");
-        if (isShot) {
-            Instantiate(bullet, transform.position, Quaternion.identity);
+        xRate = Input.GetAxisRaw("Horizontal");
+        if (xRate != 0) {
+            isMove = true;
+        } else {
+            isMove = false;
         }
+
+        if (Input.GetKey("space")) {
+            if (!jumpKeyLock) {
+                jumpKey = true;
+            } else {
+                jumpKey = false;
+            }
+        } else {
+            jumpKey = false;
+            jumpKeyLock = false;
+        }
+
+        /*isShot = Input.GetKey("space");
+        if (isShot) {
+            var shot = Instantiate(bullet, transform.position, Quaternion.identity);
+            shot.Init(45f, 10f);
+        }*/
     }
 
     private void FixedUpdate()
     {
-        dir = new Vector2(xSpeed, ySpeed).normalized;
-        rb.velocity = dir * speed;
+        vect = Vector2.zero;
+
+        if (isMove) {
+            vect.x = xRate * speed;
+            transform.localScale = new Vector2(xRate, 1f);
+        } else {
+            vect.x = 0f;
+        }
+
+        if (status == Status.GROUND && rb.velocity.y < 0f) {
+            status = Status.FALL;
+            vect.y = 0f;
+            jumpTimer = 0.1f;
+        }
+
+        switch (status) {
+            case Status.GROUND:
+                if (jumpKey) {
+                    status = Status.RISE;
+                }
+                break;
+
+            case Status.RISE:
+                jumpTimer += Time.deltaTime;
+                if (jumpKey || jumpTimer < 0.03f) {
+                    vect.y = jumpPower;
+                    vect.y -= gravity * Mathf.Pow(jumpTimer, 2f);
+                } else {
+                    jumpTimer += Time.deltaTime;
+                    vect.y = jumpPower;
+                    vect.y -= gravity * Mathf.Pow(jumpTimer, 1.4f);
+                }
+
+                if (vect.y < 0f) {
+                    status = Status.FALL;
+                    vect.y = 0f;
+                    jumpTimer = 0.1f;
+                }
+                break;
+
+            case Status.FALL:
+                jumpTimer += Time.deltaTime;
+                if (rb.velocity.y > -15f) {
+                    vect.y = 0f;
+                    vect.y = -(gravity * Mathf.Pow(jumpTimer, 2.25f));
+                } else {
+                    vect.y = -15f;
+                }
+                break;
+
+            default:
+                break;
+        }
+
+        rb.velocity = vect;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (status == Status.FALL && col.gameObject.layer == 6) {
+            status = Status.GROUND;
+            jumpTimer = 0f;
+            jumpKeyLock = true;
+        }
     }
 }
