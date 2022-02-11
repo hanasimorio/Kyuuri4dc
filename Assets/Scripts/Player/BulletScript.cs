54df614d76dfb7dfc2b3fe28d494616905ab8966
nerumoa019 <50538677+nerumoa019@users.noreply.github.com>
2022-02-12 01:02:53 +0900

    弾の修正

--- Assets/Scripts/Player/BulletScript.cs
+++ Assets/Scripts/Player/BulletScript.cs
@@ -6,12 +6,21 @@ public class BulletScript : MonoBehaviour
 {
     float speed = 12f;
     float dir;
+    float timer = 0f;
     Vector2 vect;
     Rigidbody2D rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 1.5f) {
+            Destroy(gameObject);
+        }
     }
 
     private void FixedUpdate()
@@ -27,11 +36,8 @@ public class BulletScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        Destroy(gameObject);
-    }
-
-    private void OnBecameInvisible()
-    {
-        Destroy(gameObject);
+        if (col.tag == "Enemy") {
+            Destroy(gameObject);
+        }
     }
 }
