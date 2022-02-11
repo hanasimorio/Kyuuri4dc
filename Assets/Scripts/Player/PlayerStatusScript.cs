6b710ee757b50b044edf440efa3096b6973d8899
nerumoa019 <50538677+nerumoa019@users.noreply.github.com>
2022-02-11 14:33:32 +0900

    カメラ追加など

---
+++ Assets/Scripts/Player/PlayerStatusScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamageScript : MonoBehaviour
+{
+    [SerializeField] float HP = 10f;
+    [SerializeField] float maxSP = 100f;
+    [SerializeField] float SP = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
